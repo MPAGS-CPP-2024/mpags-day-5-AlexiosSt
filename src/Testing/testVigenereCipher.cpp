61fffa7397f71f4793ab916e9afdc6dcee125936
Alexis Stampekis <alex.stabekis@gmail.com>
2024-11-28 18:38:40 +0000

    Implement the Vigenere Cipher.
    Add a class for the Vigenere cipher with two functions.
    One for setting up the key (only uppercase letters) and
    a map containing the key letter and a corresponding Caesar cipher
    object with key the position of the letter in the alphabet.
    The second applies the Vigenere cipher, by applying the Caesar cipher
    (of the previously set map, i.e. with key the position of the key letter)
    for each input letter separately.

---
+++ src/Testing/testVigenereCipher.cpp
@@ -0,0 +1,14 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include "VigenereCipher.hpp"
+
+TEST_CASE("Vigenere Cipher encryption", "[vigenere]") {
+  VigenereCipher cc{"hello"};
+  REQUIRE( cc.applyCipher("THISISQUITEALONGMESSAGESOTHEKEYWILLNEEDTOREPEATAFEWTIMES", CipherMode::Encrypt) == "ALTDWZUFTHLEWZBNQPDGHKPDCALPVSFATWZUIPOHVVPASHXLQSDXTXSZ");
+}
+
+TEST_CASE("Vigenere Cipher decryption", "[vigenere]") {
+  VigenereCipher cc{"hello"};
+  REQUIRE( cc.applyCipher("ALTDWZUFTHLEWZBNQPDGHKPDCALPVSFATWZUIPOHVVPASHXLQSDXTXSZ", CipherMode::Decrypt) == "THISISQUITEALONGMESSAGESOTHEKEYWILLNEEDTOREPEATAFEWTIMES");
+}
